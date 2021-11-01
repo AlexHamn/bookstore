9140328f81765e78870dcfa92b8a9cd17329b9b1
AlexHamn <agalexandro16@gmail.com>
2021-11-01 17:34:38 -0600

    create App file

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import Books from "./pages/Books";
+import Categories from "./pages/Categories";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <Router>
+      <header>
+        <Navbar />
+      </header>
+      <Switch>
+        <Route exact path="/Books">
+          <Books />
+        </Route>
+        <Route exact path="/Categories">
+          <Categories />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,30 +1,11 @@
 import React from "react";
 import ReactDOM from 'react-dom';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
 
-import Books from "./pages/Books";
-import Categories from "./pages/Categories";
-import Navbar from "./components/Navbar";
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <header>
-        <Navbar />
-      </header>
-      <Switch>
-        <Route exact path="/Books">
-          <Books />
-        </Route>
-        <Route exact path="/Categories">
-          <Categories />
-        </Route>
-      </Switch>
-    </Router>
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
