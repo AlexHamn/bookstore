9b70a830f8a1e1209b64354a4080f3b56e39a486
AlexHamn <agalexandro16@gmail.com>
2021-11-01 15:55:38 -0600

    create pages

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import reactDom from "react-dom";
+import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+reactDom.render(
+  <React.StrictMode>
+    <Router>
+      <Switch>
+        <Route>
+          
+        </Route>
+      </Switch>
+    </Router>
+  </React.StrictMode>
+);
