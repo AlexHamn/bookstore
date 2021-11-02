56eef0cd35473789f97e2c1a6f4ae27b42e9f9ee
AlexHamn <agalexandro16@gmail.com>
2021-11-02 14:41:14 -0600

    clear console errors

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -28,9 +28,9 @@ const AddBook = () => {
       <form onSubmit={submitBookToStore}>
         <input type="text" id="addBook" name="addBook" placeholder="new book" />
         <input type="text" id="addBookAuthor" name="addBookAuthor" placeholder="author" />
-        <select name="category" id="category">
-          <option value="Drama" selected>Drama</option>
-          <option value="Economy" selected>Economy</option>
+        <select name="category" id="category" disabled>
+          <option value="Drama">Drama</option>
+          <option value="Economy">Economy</option>
           <option value="Science Fiction">Science Fiction</option>
           <option value="Action">Action</option>
         </select>
