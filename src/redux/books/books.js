2d91e72e5f474e399a889017856b562730830f50
AlexHamn <agalexandro16@gmail.com>
2021-11-02 09:28:48 -0600

    export action creators

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -2,3 +2,12 @@ const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
 
 const initialState = [];
+
+export const addBook = payload => ({
+    type: ADD_BOOK,
+    payload
+})
+export const removeBook = payload => ({
+    type: REMOVE_BOOK,
+    payload
+})
